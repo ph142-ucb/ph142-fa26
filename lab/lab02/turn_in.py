7b6dcd7aaa175968603fa95c2927845930e7d354
Qiuran Lyu <ccccyyyylqr@gmail.com>
2026-09-01 12:14:12 -0700

    Update turn_in.py

--- lab/lab02/turn_in.py
+++ lab/lab02/turn_in.py
@@ -10,7 +10,7 @@ from os.path import isfile, join
 # Change these for each assignment
 ASSIGNMENT_RMD_PATH = "lab02.Rmd"
 COURSE_ID =  1354579
-PROGRAMMING_ASSIGNMENT_ID = 7500968
+PROGRAMMING_ASSIGNMENT_ID = 8536776
 
 # Don't change these
 PDF_PATH = ASSIGNMENT_RMD_PATH.replace('.Rmd', '.pdf')
